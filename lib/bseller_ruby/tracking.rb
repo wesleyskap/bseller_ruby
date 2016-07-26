dc816f4215407ef0f20e87c41e0fc8ccd9b3281f
Wesley Conde <wesleyskap@gmail.com>
2016-07-26 10:38:02 -0300

    feat(tracking#rest): add massive tracking list

--- lib/bseller_ruby/tracking.rb
+++ lib/bseller_ruby/tracking.rb
@@ -2,6 +2,16 @@ module BsellerRuby
   class Tracking < Base
     attr_reader :response
 
+    PATH = "/entregas/" # -> massivo{?unidadeNegocio,maxRegistros}""
+
+    def self.massive(params)
+      get("#{PATH}/massivo", params)
+    end
+
+    def self.massive_confirm(batch: nil)
+      put("#{PATH}/massivo", batch, nil)
+    end
+
     def self.list(params)
       call(:consulta_tracking, params).result
     end
--- spec/bseller_ruby/tracking_spec.rb
+++ spec/bseller_ruby/tracking_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe BsellerRuby::Tracking do
+  describe 'restful' do
+    it 'get massive list' do
+      VCR.use_cassette('tracking_massive_list') do
+        @response = BsellerRuby::Tracking.massive(unidadeNegocio: 'B2W', maxRegistros: 10)
+        binding.pry
+        expect(@response).to an_instance_of(Hash)
+      end
+    end
+
+    it 'confirm massive list' do
+      VCR.use_cassette(('tracking_confirm_massive_list') do
+        response = BsellerRuby::Tracking.massive_confirm(batch: @response['batchNumber'])
+        expect(response["message"]).to eql("Lote confirmado com sucesso")
+      end
+    end
+  end
+
   describe '.list' do
     it 'get tracking list' do
       VCR.use_cassette('tracking_list') do
