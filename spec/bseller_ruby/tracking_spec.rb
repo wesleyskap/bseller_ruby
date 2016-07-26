19dbdd890ef67f9e08f89dc04ff9fcdd432b3f39
Wesley Conde <wesleyskap@gmail.com>
2016-07-26 10:54:31 -0300

    remove binding

--- spec/bseller_ruby/tracking_spec.rb
+++ spec/bseller_ruby/tracking_spec.rb
@@ -5,7 +5,6 @@ describe BsellerRuby::Tracking do
     it 'get massive list' do
       VCR.use_cassette('tracking_massive_list') do
         @response = BsellerRuby::Tracking.massive(unidadeNegocio: 'B2W', maxRegistros: 10)
-        binding.pry
         expect(@response).to an_instance_of(Hash)
       end
     end
